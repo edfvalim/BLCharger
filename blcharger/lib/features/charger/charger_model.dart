5c8eda1360364394b9d1b19af97d371a8586c887
edfvalim <edvalim@icloud.com>
2023-07-30 18:51:47 -0400

    [STYLE](ChargerModel): new structure

--- blcharger/lib/features/charger/charger_model.dart
+++ blcharger/lib/features/charger/charger_model.dart
@@ -1,14 +1,16 @@
 class ChargerModel {
+  final String name; // charger name
   final String id; // charger unique identifier
-  final String chargerImagePath; // charger image path
-  final String chargerDescription; // charger description
+  final String imagePath; // charger image path
+  final String description; // charger description
   final double pricePerKwh; // charger current price per kWh
   bool isAvailable; // charger availability
 
   ChargerModel({
+    required this.name,
     required this.id,
-    required this.chargerImagePath,
-    required this.chargerDescription,
+    required this.imagePath,
+    required this.description,
     required this.pricePerKwh,
     this.isAvailable = true,
   });
